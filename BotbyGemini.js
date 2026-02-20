f928ec3f66922a84844a7e52931b19cc6a0dcb70
ghostreacon37-dev <ghostreacon37@gmail.com>
2026-02-21 00:30:43 +0530

    Working bot

--- BotbyGemini.js
+++ BotbyGemini.js
@@ -6,9 +6,9 @@ puppeteer.use(StealthPlugin());
 
 class EternalGhostBotV21 {
     constructor() {
-        this.targetWord = "LearnWithBlog.xyz";
-        this.targetDomain = "learnwithblog.xyz";
-        this.referrerUrl = "https://x.com/GhostReacondev/status/2013213212175724818";
+        this.targetWord = "learnblogs.online";
+        this.targetDomain = "learnblogs.online";
+        this.referrerUrl = "https://x.com/GhostReacondev/status/2024921591520641247?s=20";
         
         // --- ALL 20+ DEVICES PRESERVED ---
         this.devices = [
